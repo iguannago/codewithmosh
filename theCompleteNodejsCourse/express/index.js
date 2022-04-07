08534492d6899002efd75643717515e9f230d1de
David Crespo <david.crespo@sky.uk>
2022-04-07 13:18:57 +0100

    add put endpoint, add validation, refactor prod code

--- theCompleteNodejsCourse/express/index.js
+++ theCompleteNodejsCourse/express/index.js
@@ -27,33 +27,73 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id == req.params.id);
-    if (course == undefined) res.status(404).send(`course with id=${req.params.id} is not found`);
+    if (course == undefined) sendError(res, 404, `course with id=${req.params.id} is not found`);
     
     res.json(course);
 });
 
 app.post('/api/courses', (req, res) => {
-    const schema = Joi.object({
-        name: Joi.string().required(),
-        id: Joi.number().required()
-    });
-
-    const {error} = schema.validate(req.body);
-    
+    const {error} = reqBodySchema.validate(req.body);
     if (error != undefined) {
-        res.status(400).json({
-            "error": error
-        });
-        return; 
+        sendError(res, 400, error);
     }
 
     const course = req.body;
-    console.log(`course added successfully: ${JSON.stringify(course)}`);
     courses.push(course);
-    res.json(course);
+    
+    res.json({
+        'message': `course added successfully`,
+        'course': course
+    });
 });
+
+function validateReqBody(reqBody, res) {
+    const reqBodySchema = Joi.object({
+        id: Joi.number().required(),
+        name: Joi.string().required()
+    });
+    let {error} = reqBodySchema.validate(reqBody);
+    if (error != undefined) {
+        sendError(res, 400, error);
+        return;    
+    }
+}
+
+function validateIdReqParam(idReqParam,res) {
+    const idReqParamSchema = Joi.number().required();
+    let {error} = idReqParamSchema.validate(idReqParam);
+    if (error != undefined) {
+        sendError(res, 400, error);
+        return;    
+    }
+}
+
+app.put('/api/courses/:id', (req, res) => {
+    validateIdReqParam(req.params.id, res);
+    validateReqBody(req.body, res);
+   
+    const course = courses.find(c => c.id == req.params.id);
+    if (course == undefined ) {
+        sendError(res, 404, `course with id=${req.params.id} is not found`);
+        return ;
+    }
+
+    const courseIndex = courses.findIndex(e => e.id == req.params.id);
+    courses[courseIndex] = req.body;
+    res.json({
+        'message': `course updated successfully`,
+        'course': courses[courseIndex]
+    });
+});
+
+function sendError(res, errorCode, errorMsg) {
+    res.status(errorCode).json({ 
+        'error': errorMsg
+    });
+}
 
 const portNumber = process.env.PORT | 3000;
 app.listen(portNumber, ()=> {
     console.log(`Listening on port ${portNumber} ...`);
 });
+
