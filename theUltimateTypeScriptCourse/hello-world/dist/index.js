493080bf83ae0b32c94f800b7a26f6108d76a4a0
david.crespo <david.crespo@hmcts.net>
2022-05-31 16:13:34 +0100

    add hellow world

---
+++ theUltimateTypeScriptCourse/hello-world/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 20;
+if (age < 15)
+    age += 10;
+console.log(`your age is: ${age}`);
+//# sourceMappingURL=index.js.map
---
+++ theUltimateTypeScriptCourse/hello-world/src/index.ts
@@ -0,0 +1,5 @@
+let age: number = 20;
+
+if (age < 15) age += 10;
+
+console.log(`your age is: ${age}`);
