f31f8bcd77bcf0e3f6f447810f8ebb671935d80b
david.crespo <david.crespo@hmcts.net>
2022-05-31 19:08:31 +0100

    add tuples

--- theUltimateTypeScriptCourse/hello-world/dist/index.js
+++ theUltimateTypeScriptCourse/hello-world/dist/index.js
@@ -3,9 +3,12 @@ let age = 20;
 let sales = 123456789;
 let course = 'TypeScript';
 let numbers = [1, 2, 3];
+let user = [1, 'David'];
 if (age < 15)
     age += 10;
-numbers.forEach(n => console.log(n));
+numbers.forEach((n) => console.log(n));
+console.log(`user: ${user[0]}`);
+console.log(`user: ${user[1]}`);
 console.log(`your age is: ${age}`);
 console.log(`your sales is: ${sales}`);
 console.log(`your course is: ${course}`);
--- theUltimateTypeScriptCourse/hello-world/src/index.ts
+++ theUltimateTypeScriptCourse/hello-world/src/index.ts
@@ -1,11 +1,17 @@
+import { ModuleDetectionKind } from 'typescript';
+
 let age = 20;
 let sales = 123_456_789;
 let course = 'TypeScript';
 let numbers = [1, 2, 3];
+let user: [number, string] = [1, 'David'];
 
 if (age < 15) age += 10;
 
-numbers.forEach(n => console.log(n));
+numbers.forEach((n) => console.log(n));
+
+console.log(`user: ${user[0]}`);
+console.log(`user: ${user[1]}`);
 
 console.log(`your age is: ${age}`);
 console.log(`your sales is: ${sales}`);
