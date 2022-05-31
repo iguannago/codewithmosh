b813149f9f9de8708cb5e767894379c3a01ed78e
david.crespo <david.crespo@hmcts.net>
2022-05-31 16:24:15 +0100

    add function

--- theUltimateTypeScriptCourse/hello-world/dist/index.js
+++ theUltimateTypeScriptCourse/hello-world/dist/index.js
@@ -1,6 +1,14 @@
 "use strict";
 let age = 20;
+let sales = 123456789;
+let course = 'TypeScript';
 if (age < 15)
     age += 10;
 console.log(`your age is: ${age}`);
+console.log(`your sales is: ${sales}`);
+console.log(`your course is: ${course}`);
+render('myDocument.txt');
+function render(document) {
+    console.log(`document is: ${document}`);
+}
 //# sourceMappingURL=index.js.map
--- theUltimateTypeScriptCourse/hello-world/src/index.ts
+++ theUltimateTypeScriptCourse/hello-world/src/index.ts
@@ -1,5 +1,15 @@
-let age: number = 20;
+let age = 20;
+let sales = 123_456_789;
+let course = 'TypeScript';
 
 if (age < 15) age += 10;
 
 console.log(`your age is: ${age}`);
+console.log(`your sales is: ${sales}`);
+console.log(`your course is: ${course}`);
+
+render('myDocument.txt');
+
+function render(document: String) {
+  console.log(`document is: ${document}`);
+}
