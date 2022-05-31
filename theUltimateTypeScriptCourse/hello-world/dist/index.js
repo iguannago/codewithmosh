9b63b37fe99c1e00f739dcec487bad0706e19966
david.crespo <david.crespo@hmcts.net>
2022-05-31 18:50:10 +0100

    array

--- theUltimateTypeScriptCourse/hello-world/dist/index.js
+++ theUltimateTypeScriptCourse/hello-world/dist/index.js
@@ -2,11 +2,14 @@
 let age = 20;
 let sales = 123456789;
 let course = 'TypeScript';
+let numbers = [1, 2, 3];
 if (age < 15)
     age += 10;
+numbers.forEach(n => console.log(n));
 console.log(`your age is: ${age}`);
 console.log(`your sales is: ${sales}`);
 console.log(`your course is: ${course}`);
+console.log(`your array of numbers is: ${numbers}`);
 render('myDocument.txt');
 function render(document) {
     console.log(`document is: ${document}`);
--- theUltimateTypeScriptCourse/hello-world/src/index.ts
+++ theUltimateTypeScriptCourse/hello-world/src/index.ts
@@ -1,12 +1,16 @@
 let age = 20;
 let sales = 123_456_789;
 let course = 'TypeScript';
+let numbers = [1, 2, 3];
 
 if (age < 15) age += 10;
+
+numbers.forEach(n => console.log(n));
 
 console.log(`your age is: ${age}`);
 console.log(`your sales is: ${sales}`);
 console.log(`your course is: ${course}`);
+console.log(`your array of numbers is: ${numbers}`);
 
 render('myDocument.txt');
 
