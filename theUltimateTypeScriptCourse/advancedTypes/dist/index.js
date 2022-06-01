9a9aa0b8ccb424a9968228a613139fec491b5165
david.crespo <david.crespo@hmcts.net>
2022-06-01 16:10:29 +0100

    add type alias

--- theUltimateTypeScriptCourse/advancedTypes/dist/index.js
+++ theUltimateTypeScriptCourse/advancedTypes/dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
-console.log('advance types');
+console.log('Advanced types module');
+let user = {
+    id: 1,
+    name: 'David',
+    retire: (date) => console.log(date),
+};
+let userType = {
+    id: 1,
+    name: 'David2',
+    retire: (date) => console.log(date),
+};
+console.log(`user type: ${JSON.stringify(userType.retire(new Date()))}`);
 //# sourceMappingURL=index.js.map
--- theUltimateTypeScriptCourse/advancedTypes/src/index.ts
+++ theUltimateTypeScriptCourse/advancedTypes/src/index.ts
@@ -1,1 +1,25 @@
-console.log('advance types');
+console.log('Advanced types module');
+
+let user: {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: 'David',
+  retire: (date: Date) => console.log(date),
+};
+
+type User = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let userType: User = {
+  id: 1,
+  name: 'David2',
+  retire: (date: Date) => console.log(date),
+};
+
+console.log(`user type: ${JSON.stringify(userType)}`);
