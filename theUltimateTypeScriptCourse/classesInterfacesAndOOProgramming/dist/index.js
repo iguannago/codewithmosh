e456508b5ef1609915a9897054904ce99a32a4d7
david.crespo <david.crespo@hmcts.net>
2022-06-02 19:06:07 +0100

    create classes and objects

--- theUltimateTypeScriptCourse/classesInterfacesAndOOProgramming/dist/index.js
+++ theUltimateTypeScriptCourse/classesInterfacesAndOOProgramming/dist/index.js
@@ -1,3 +1,19 @@
 "use strict";
 console.log('Classes, Interfaces and OO programming module');
+class Account {
+    constructor(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+    deposit(amount) {
+        if (amount <= 0)
+            throw new Error(`invalid amount: ${amount}`);
+        this.balance += amount;
+    }
+}
+let myAccount = new Account(1, 'David', 100);
+console.log(`myAccount: ${JSON.stringify(myAccount)}`);
+myAccount.deposit(20);
+console.log(`myAccount: ${JSON.stringify(myAccount)}`);
 //# sourceMappingURL=index.js.map
--- theUltimateTypeScriptCourse/classesInterfacesAndOOProgramming/src/index.ts
+++ theUltimateTypeScriptCourse/classesInterfacesAndOOProgramming/src/index.ts
@@ -1,2 +1,24 @@
 console.log('Classes, Interfaces and OO programming module');
 
+class Account {
+  id: number;
+  owner: string;
+  balance: number;
+
+  constructor(id: number, owner: string, balance: number) {
+    this.id = id;
+    this.owner = owner;
+    this.balance = balance;
+  }
+
+  deposit(amount: number): void {
+    if (amount <= 0) throw new Error(`invalid amount: ${amount}`);
+    this.balance += amount;
+  }
+}
+
+let myAccount: Account = new Account(1, 'David', 100);
+
+console.log(`myAccount: ${JSON.stringify(myAccount)}`);
+myAccount.deposit(20);
+console.log(`myAccount: ${JSON.stringify(myAccount)}`);
