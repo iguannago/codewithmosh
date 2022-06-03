1165d7d27b673c4b730b2e5c1f575eba6ef28e5d
david.crespo <david.crespo@hmcts.net>
2022-06-03 12:21:50 +0100

    access modifiers

--- theUltimateTypeScriptCourse/classesInterfacesAndOOProgramming/dist/index.js
+++ theUltimateTypeScriptCourse/classesInterfacesAndOOProgramming/dist/index.js
@@ -4,16 +4,20 @@ class Account {
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     deposit(amount) {
         if (amount <= 0)
             throw new Error(`invalid amount: ${amount}`);
-        this.balance += amount;
+        this._balance += amount;
+    }
+    get balance() {
+        return this._balance;
     }
 }
-let myAccount = new Account(1, 'David', 100);
+let myAccount = new Account(1, 'David', 0);
 console.log(`myAccount: ${JSON.stringify(myAccount)}`);
 myAccount.deposit(20);
 console.log(`myAccount: ${JSON.stringify(myAccount)}`);
+myAccount.balance;
 //# sourceMappingURL=index.js.map
--- theUltimateTypeScriptCourse/classesInterfacesAndOOProgramming/src/index.ts
+++ theUltimateTypeScriptCourse/classesInterfacesAndOOProgramming/src/index.ts
@@ -3,22 +3,27 @@ console.log('Classes, Interfaces and OO programming module');
 class Account {
   readonly id: number;
   readonly owner: string;
-  balance: number;
+  private _balance: number;
 
   constructor(id: number, owner: string, balance: number) {
     this.id = id;
     this.owner = owner;
-    this.balance = balance;
+    this._balance = balance;
   }
 
   deposit(amount: number): void {
     if (amount <= 0) throw new Error(`invalid amount: ${amount}`);
-    this.balance += amount;
+    this._balance += amount;
+  }
+
+  public get balance(): number {
+    return this._balance;
   }
 }
 
-let myAccount: Account = new Account(1, 'David', 100);
+let myAccount: Account = new Account(1, 'David', 0);
 
 console.log(`myAccount: ${JSON.stringify(myAccount)}`);
 myAccount.deposit(20);
 console.log(`myAccount: ${JSON.stringify(myAccount)}`);
+myAccount.balance;
