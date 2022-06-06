d52e78c3a130fb7b92fe68103a8569e93eb960aa
david.crespo <david.crespo@hmcts.net>
2022-06-06 19:04:26 +0100

    generic function

--- theUltimateTypeScriptCourse/generics/dist/index.js
+++ theUltimateTypeScriptCourse/generics/dist/index.js
@@ -1,10 +1,22 @@
 "use strict";
+console.log('Generic class');
 class KeyValuePair {
     constructor(key, value) {
         this.key = key;
         this.value = value;
     }
+    wrapInArray(value) {
+        return [value];
+    }
 }
 let myKeyValuePair = new KeyValuePair(1, 'a');
 console.log(myKeyValuePair);
+console.log(myKeyValuePair.wrapInArray(1));
+console.log(myKeyValuePair.wrapInArray('1'));
+console.log('\n\nGeneric function');
+function wrapInArray(value) {
+    return [value];
+}
+console.log(wrapInArray(1));
+console.log(wrapInArray('1'));
 //# sourceMappingURL=index.js.map
--- theUltimateTypeScriptCourse/generics/src/index.ts
+++ theUltimateTypeScriptCourse/generics/src/index.ts
@@ -1,6 +1,22 @@
+console.log('Generic class');
 class KeyValuePair<T, V> {
   constructor(public key: T, public value: V) {}
+
+  wrapInArray<T>(value: T) {
+    return [value];
+  }
 }
 
 let myKeyValuePair: KeyValuePair<number, string> = new KeyValuePair(1, 'a');
 console.log(myKeyValuePair);
+console.log(myKeyValuePair.wrapInArray(1));
+console.log(myKeyValuePair.wrapInArray('1'));
+
+//generic function
+console.log('\n\nGeneric function');
+
+function wrapInArray<T>(value: T): [T] {
+  return [value];
+}
+console.log(wrapInArray(1));
+console.log(wrapInArray('1'));
