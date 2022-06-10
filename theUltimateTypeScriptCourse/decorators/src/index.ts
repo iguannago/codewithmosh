959f6201db5e70569c5684b040181de2710866b2
david.crespo <david.crespo@hmcts.net>
2022-06-10 16:51:11 +0100

    create decorator

--- theUltimateTypeScriptCourse/decorators/src/index.ts
+++ theUltimateTypeScriptCourse/decorators/src/index.ts
@@ -1,2 +1,14 @@
 //what are decorators
 console.log('what are decorators');
+
+//class decorator
+function Component(constructor: Function) {
+  console.log('component decorator called');
+  constructor.prototype.uniqueId = Date.now();
+  constructor.prototype.insertInDom = () => {
+    console.log('Inserting the component in the DOM');
+  };
+}
+
+@Component
+class ProfileComponent {}
