cbed21c2046d26e648ad5666beb34a6349e7f23b
david.crespo <david.crespo@hmcts.net>
2022-06-15 12:43:41 +0100

    using the effect hook

---
+++ theUltimateTypeScriptCourse/react2/reminder-app/reminders-app/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import ReminderList from './components/ReminderList';
+import Reminder from './models/Reminder';
+import reminderService from './services/reminder';
+
+function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders();
+    setReminders(reminders);
+  };
+
+  return (
+    <div className='App'>
+      <ReminderList items={reminders} />
+    </div>
+  );
+}
+
+export default App;
---
+++ theUltimateTypeScriptCourse/react2/reminder-app/reminders-app/src/services/reminder.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import Reminder from '../models/Reminder';
+
+class ReminderService {
+  http = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com/',
+  });
+
+  async getReminders() {
+    const response = await this.http.get<Reminder[]>('/todos');
+    return response.data;
+  }
+
+  async addReminder(title: string) {
+    const response = await this.http.post<Reminder>('/todos', { title });
+    return response.data;
+  }
+
+  async removeReminder(id: number) {
+    const response = await this.http.delete('/todos/' + id);
+    return response.data;
+  }
+}
+
+export default new ReminderService();
