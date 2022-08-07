ae634affa0e088bbb70b53f6793dc316e0df65df
david.crespo <david.crespo@hmcts.net>
2022-08-07 16:33:00 +0100

    format code

--- theUltimateTypeScriptCourse/react2/reminder-app/reminders-app/src/App.tsx
+++ theUltimateTypeScriptCourse/react2/reminder-app/reminders-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import NewReminder from './components/NewReminder';
 import ReminderList from './components/ReminderList';
