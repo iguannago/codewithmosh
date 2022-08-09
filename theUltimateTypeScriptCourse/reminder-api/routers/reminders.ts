0aa70efae0f18bea839b33f269dddc316fdf97d9
david.crespo <david.crespo@hmcts.net>
2022-08-09 19:19:14 +0100

    building an API

--- theUltimateTypeScriptCourse/reminder-api/routers/reminders.ts
+++ theUltimateTypeScriptCourse/reminder-api/routers/reminders.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
 import CreateReminderDto from '../dtos/reminderDto';
+import Reminder from '../models/reminder';
 const router = Router();
+const reminderList: Reminder[] = [];
 
 router.get('/', (req, res) => {
-  res.send('List of reminders');
+  res.json(reminderList);
 });
 
 router.post('/', (req, res) => {
   const { title } = req.body as CreateReminderDto;
-  res.json(title);
+  const reminder = new Reminder(title);
+  reminderList.push(reminder);
+  res.status(201).json(reminder);
 });
 
 export default router;
