ec4231fc811e8c707239b4671e13323ed385fbcd
david.crespo <david.crespo@hmcts.net>
2022-08-09 18:59:30 +0100

    creating a router

--- theUltimateTypeScriptCourse/reminder-api/index.ts
+++ theUltimateTypeScriptCourse/reminder-api/index.ts
@@ -1,4 +1,8 @@
 import express from 'express';
+import routerReminder from './routers/reminders'
 const app = express();
 
+app.use('/reminders', routerReminder)
+
 app.listen(8000, () => console.log('server running...'));
+
